062826d3d7578273b4f3fd815261c847253d77d4
michaelvivar <michaelvictorvivar@gmail.com>
2016-09-11 01:13:58 +0800

    update 8

--- EnSys/BL/Services/ClassScheduleService.cs
+++ EnSys/BL/Services/ClassScheduleService.cs
@@ -5,8 +5,6 @@ using DL.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BL.Services
 {
--- EnSys/BL/Services/CourseService.cs
+++ EnSys/BL/Services/CourseService.cs
@@ -2,11 +2,8 @@
 using BL.Interfaces;
 using DL;
 using DL.Entities;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Util.Enums;
 
 namespace BL.Services
