00a7718d10a7701692c547895f785d692df1b836
michaelvivar <michaelvictorvivar@gmail.com>
2016-09-21 07:25:29 +0800

    minor changes

--- EnSys/UI/Controllers/ClassScheduleController.cs
+++ EnSys/UI/Controllers/ClassScheduleController.cs
@@ -59,7 +59,10 @@ namespace UI.Controllers
         {
             return Transaction.Scope(scope => scope.Service<ClassScheduleService, ActionResult>(service =>
             {
-                return PartialView("Table", service.GetClassesFiltered(Convert.ToInt32(day), Convert.ToInt32(instructor), Convert.ToInt32(subject), Convert.ToInt32(section), Convert.ToInt32(room)).Select(o => MapDtoToModel(o)));
+                return PartialView("Table", service.GetClassesFiltered(
+                    Convert.ToInt32(day), Convert.ToInt32(instructor),
+                    Convert.ToInt32(subject), Convert.ToInt32(section),
+                    Convert.ToInt32(room)).Select(o => MapDtoToModel(o)));
             }));
 
         }
