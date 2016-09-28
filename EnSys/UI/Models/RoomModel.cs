3ebd90aa6a78d8d1432ec191d2d396979619e425
michaelvivar <michaelvictorvivar@gmail.com>
2016-09-28 17:20:15 +0800

    removed validator

--- EnSys/UI/Models/RoomModel.cs
+++ EnSys/UI/Models/RoomModel.cs
@@ -9,41 +9,13 @@ using Util.Enums;
 
 namespace UI.Models
 {
-    public class RoomModel :  IRoom
+    public class RoomModel : IRoom
     {
         public int Id { get; set; }
         public string Number { get; set; }
-        public int? Capacity { get; set; }
+        public int Capacity { get; set; }
         public string Remarks { get; set; }
-        public Status? Status { get; set; }
+        public Status Status { get; set; }
         public int StatusId { get { return Convert.ToInt32(Status); } }
-    }
-
-    public class ValidateRoomModel : RoomModel, IValidatableObject
-    {
-        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
-        {
-            IValidationResultHelper<RoomModel> helper = new ValidationResultHelper<RoomModel>(this);
-
-            helper.Validate(model => model.Number).Required(true).ErrorMsg("Number field is required");
-
-            helper.Validate(model => model.Capacity).Required(true).GreaterThan(0).ErrorMsg("Capacity must be greater than 0(Zero)");
-
-            helper.Validate(model => model.Status).Required(true).GreaterThan(0).ErrorMsg("Status field is required");
-
-            if (!helper.Failed)
-            {
-                Transaction.Scope(scope => scope.Service<RoomValidatorService>(service =>
-                {
-                    helper.Validate(model => model.Number).Required(true).IF(service.CheckRoomNumberExists(Id, Number)).ErrorMsg(string.Format("Room number/name \"{0}\" is already exists!", Number));
-                }));
-            }
-
-            if (helper.Failed)
-            {
-                foreach (var error in helper.Errors)
-                    yield return error;
-            }
-        }
     }
 }
